cbc024a97f63e74149e22ba48eeb6dffb1f9e5aa
Muslim- <googojoseh@gmail.com>
2026-03-11 05:38:14 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,53 +1,34 @@
 from flask import Flask, request, jsonify
 import yt_dlp
-import uuid
 import os
-import json
 
+# Flask app samee
 app = Flask(__name__)
 
-HISTORY_FILE = "history.json"
+@app.route("/")
+def home():
+    return "Downloader API Running"
 
-def load_history():
-    if not os.path.exists(HISTORY_FILE):
-        return []
-    with open(HISTORY_FILE,"r") as f:
-        return json.load(f)
-
-def save_history(data):
-    with open(HISTORY_FILE,"w") as f:
-        json.dump(data,f)
-
-@app.route("/download",methods=["POST"])
+@app.route("/download", methods=["POST"])
 def download():
 
     data = request.json
     url = data["url"]
 
-    video_id = str(uuid.uuid4())
-    filename = f"video_{video_id}.mp4"
-
     ydl_opts = {
-        "format":"best",
-        "outtmpl":filename
+        "format": "best",
+        "outtmpl": "video.%(ext)s"
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
-
-    history = load_history()
-    history.append({
-        "url":url,
-        "file":filename
-    })
-    save_history(history)
+        info = ydl.extract_info(url, download=True)
+        filename = ydl.prepare_filename(info)
 
     return jsonify({
-        "video":filename
+        "video": filename
     })
 
-@app.route("/history")
-def history():
-    return jsonify(load_history())
+port = int(os.environ.get("PORT",3000))
 
-app.run(host="0.0.0.0",port=3000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=port)

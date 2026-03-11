c270a5dd7b6ed9b3b1121a67bedf9b391b1c95be
Muslim- <googojoseh@gmail.com>
2026-03-11 06:06:13 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,34 +1,25 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, send_file
 import yt_dlp
 import os
 
-# Flask app samee
 app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "Downloader API Running"
 
 @app.route("/download", methods=["POST"])
 def download():
-
-    data = request.json
-    url = data["url"]
+    data = request.get_json()
+    url = data.get("url")
 
     ydl_opts = {
-        "format": "best",
-        "outtmpl": "video.%(ext)s"
+        'outtmpl': 'downloads/%(title)s.%(ext)s',
+        'format': 'mp4'
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         info = ydl.extract_info(url, download=True)
         filename = ydl.prepare_filename(info)
 
-    return jsonify({
-        "video": filename
-    })
-
-port = int(os.environ.get("PORT",3000))
+    return send_file(filename, as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=port)
+    os.makedirs("downloads", exist_ok=True)
+    app.run(host="0.0.0.0", port=5000)

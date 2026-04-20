c7426a752fa3df55655d178d3b0f51162b5dc09d
Muslim- <googojoseh@gmail.com>
2026-04-20 18:45:45 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,23 +1,21 @@
-from flask import Flask, request, send_file, render_template, jsonify
+from flask import Flask, request, send_file, jsonify, render_template
 import yt_dlp
 import os
 import uuid
 
 app = Flask(__name__)
 
-# create downloads folder
 os.makedirs("downloads", exist_ok=True)
 
-@app.route("/", methods=["GET"])
+@app.route("/")
 def home():
     return render_template("index.html")
 
 
-@app.route("/download", methods=["POST"])
+@app.route("/download", methods=["GET"])
 def download():
 
-    data = request.get_json()
-    url = data.get("url")
+    url = request.args.get("url")
 
     if not url:
         return jsonify({"error": "No URL provided"})
@@ -36,10 +34,18 @@ def download():
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             ydl.download([url])
 
-        return send_file(filename, as_attachment=True)
+        return jsonify({
+            "video": f"/file/{os.path.basename(filename)}"
+        })
 
     except Exception as e:
         return jsonify({"error": str(e)})
+
+
+@app.route("/file/<name>")
+def file(name):
+    path = f"downloads/{name}"
+    return send_file(path, as_attachment=True)
 
 
 if __name__ == "__main__":
